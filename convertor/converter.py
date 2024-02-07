d091a65578718d43c314ad2c25868bac0f2e15f0
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-02-07 11:16:04 -0600

    added create_segment_labels method

--- convertor/converter.py
+++ convertor/converter.py
@@ -1,10 +1,14 @@
 import labelbox as lb
+import labelbox.types as lb_types
+from uuid import uuid4
 
 class lb_converter:
     def __init__(self, project_id, client, ontology_mapping) -> None:
         self.client: lb.Client = client
         self.project: str = project_id
-        self.ontology_mapping: list[dict[str:str]] = ontology_mapping
+        self.ontology_mapping: dict[str:str] = ontology_mapping
+        self.labels = []
+        
     def export_data_row(self) -> list[dict[str:str]]:
         project = self.client.get_project(self.project)
         
@@ -45,3 +49,92 @@ class lb_converter:
                 data_rows.append(output.json_str)
         
         return data_rows
+    
+    def import_ground_truths(self, mal_job_name = f"mal-{str(uuid4())}"):
+        if len(self.labels) == 0:
+            raise Exception("No labels to import")
+        upload_job = lb.LabelImport.create_from_objects(
+                        client = self.client,
+                        project_id = self.project,
+                        name=mal_job_name,
+                        labels=self.labels
+                        )
+        upload_job.wait_until_done()
+        
+        return upload_job
+    
+    def import_predictions(self, mal_job_name = f"mal-{str(uuid4())}"):
+        if len(self.labels) == 0:
+            raise Exception("No labels to import")
+        upload_job = lb.MALPredictionImport.create_from_objects(
+                        client = self.client,
+                        project_id = self.project,
+                        name=mal_job_name,
+                        predictions=self.labels
+                        )
+        upload_job.wait_until_done()
+        
+        return upload_job
+
+
+    def create_bbox_labels(self, results: list[dict[str:str]] ):
+        """Creates bbox labels that can then be imported
+
+        Args:
+            results (list[dict[str:str]]): [{
+                                            global_key: <Global_Key>, 
+                                            predictions: [{
+                                                           answer: <model_answer_name>, 
+                                                           points: {start_x, start_y, end_x, end_y}
+                                                         }]
+                                           }]
+        """
+        for result in results:
+            annotations = []
+            if "predictions" in result:
+                for prediction in result["predictions"]:
+                    lb_name = self.ontology_mapping[prediction["answer"]]
+                    bbox_source = lb_types.ObjectAnnotation(
+                        name=lb_name,
+                        value=lb_types.Rectangle(
+                            start=lb_types.Point(x=prediction["start_x"], y=prediction["start_y"]),
+                            end=lb_types.Point(x=prediction["end_x"], y=prediction["end_y"])
+                        ))
+                    annotations.append(bbox_source)
+                self.labels.append(
+                    lb_types.Label(data=lb_types.ImageData(global_key=result["global_key"]), annotations=annotations)
+                    )
+    def create_segment_labels(self, results: list[dict[str:str]] ):
+        """Creates segment labels that can then be imported
+
+        Args:
+            results (list[dict[str:str]]): [{
+                                            global_key: <Global_Key>, 
+                                            predictions: [{
+                                                           answer: <model_answer_name>, 
+                                                           mask: <byte_array>,
+                                                           color(optional): <rgb tuple> 
+                                                         }]
+                                            
+                                           }]
+        """
+        for result in results:
+            annotations = []
+            if "predictions" in result:
+                for prediction in result["predictions"]:
+                    color = (255,255,255)
+                    if "color" in prediction:
+                        color = prediction["color"]
+                    lb_name = self.ontology_mapping[prediction["answer"]]
+                    mask_data = lb_types.MaskData(im_bytes=prediction["mask"])
+                    mask_annotation = lb_types.ObjectAnnotation(
+                        name=lb_name,
+                        value=lb_types.Mask(
+                            mask=mask_data,
+                            color=color
+                        ))
+                    annotations.append(mask_annotation)
+                self.labels.append(
+                    lb_types.Label(data=lb_types.ImageData(global_key=result["global_key"]), annotations=annotations)
+                    )
+        
